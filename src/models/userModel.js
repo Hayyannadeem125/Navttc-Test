5f99047bc4602159264be64125030aeb1c4d9602
HAyyan Ahmed Nadeem <hayyannadeem125@gmail.com>
2024-10-04 17:09:09 +0500

    my first commit

---
+++ src/models/userModel.js
@@ -0,0 +1,46 @@
+// user model
+import mongoose, { model } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  // address: {
+  //     street: String,
+  //     city: String,
+  //     state: String,
+  //     zipCode: String,
+  //     country: String,
+  // },
+  // image:{
+  // type: String,
+  // },
+  role: {
+    type: String,
+    enum: ["customer", "admin"],
+    default: "customer",
+  },
+});
+
+let user = mongoose.model("user", userSchema);
+export default user;
